15348ce69216c7e6144bc698cc33709b1b173a98
giorgi <gioarqania84@gmail.com>
2025-04-09 13:52:34 +0400

    search in header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.webp';
 
 function Header() {
-  return (
-    <header>
-      <img src={logo} alt="Logo" />
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
+  const [showSearch, setShowSearch] = useState(false);
+  const inputRef = useRef(null);
 
-      <div className="icons">
-        <i className="fa fa-search"></i>
-        <Link to="/cart"><i className="fa fa-shopping-cart"></i></Link>
-        <i className="fa fa-bars"></i>
-      </div>
-    </header>
+  useEffect(() => {
+    if (showSearch && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [showSearch]);
+
+  return (
+    <>
+      <header className="header">
+        <img src={logo} alt="Logo" />
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        <div className="icons">
+          <i
+            className="fa fa-search"
+            onClick={() => setShowSearch((prev) => !prev)}
+          ></i>
+          <Link to="/cart">
+            <i className="fa fa-shopping-cart"></i>
+          </Link>
+          <i className="fa fa-bars"></i>
+        </div>
+      </header>
+
+        {showSearch && (
+          <input
+            ref={inputRef}
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        )}
+    </>
   );
 }
 
