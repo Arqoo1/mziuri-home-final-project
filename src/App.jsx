2942d388a6d9c8509f0f9f2bc248dedee1384f67
giorgi <gioarqania84@gmail.com>
2025-04-09 15:07:33 +0400

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './styles/styles.scss';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/styles.scss";
 
-import Header from './layouts/Header';
-import Main from './layouts/Main';
-import Footer from './layouts/Footer';
-import setTitleFromPath from "./utils/setTitle";
+import Header from "./layouts/Header";
+import Main from "./layouts/Main";
+import Footer from "./layouts/Footer";
+import { TitleUpdater } from "./utils/setTitle";
 
-import Home from './routes/Home';
-import About from './routes/About';
-import Contact from './routes/Contact';
-import Shop from './routes/Shop';
-import Cart from './routes/Cart';
-import NotFound from './routes/NotFound';
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Shop from "./routes/Shop";
+import SingleProduct from "./routes/SingleProduct";
+import Cart from "./routes/Cart";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
+import Profile from "./routes/Profile";
+import Wishlist from "./routes/Wishlist";
+import Checkout from "./routes/Checkout";
+import Compare from "./routes/Compare";
+import Blog from "./routes/Blog";
+import NotFound from "./routes/NotFound";
 
 function App() {
-  useEffect(() => {
-    setTitleFromPath();
-  }, []);
-
   return (
     <Router>
       <Header />
       <Main>
+        <TitleUpdater />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:id" element={<SingleProduct />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/compare" element={<Compare />} />
+          <Route path="/blog" element={<Blog />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Main>
--- src/components/RouteBanner.jsx
+++ src/components/RouteBanner.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function RouteBanner({ page }) {
   return (
     <section className="route-banner">
-      <h1>{page}</h1>
+      <h3>
+        <Link to="/">Home</Link>
+        <i className="fas fa-angle-right mx-2"></i>
+        <span>{page}</span>
+      </h3>
     </section>
   );
 }
