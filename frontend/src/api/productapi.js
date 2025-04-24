c80ccfff98d2fd04cc354c27bb689465b5846a1c
giorgi <gioarqania84@gmail.com>
2025-04-24 21:12:36 +0400

    .

--- frontend/src/api/productapi.js
+++ frontend/src/api/productapi.js
@@ -2,15 +2,12 @@ const API_URL = "http://localhost:5000/api/products";
 
 const fetchProductData = async () => {
   try {
-    const response = await fetch(API_URL); // Using fetch to make the request
-    if (!response.ok) { // Check if the response is successful (status code 200-299)
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json(); // Parse the JSON response
+    const response = await fetch(API_URL); 
+    const data = await response.json(); 
     return data;
   } catch (error) {
     console.error('Error fetching product data:', error);
-    return []; // Return an empty array in case of an error
+    return [];
   }
 };
 
